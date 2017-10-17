4dcbaf8c755fed6c7386470ef3a0b660f33c98e9
Jason Slusarchuk <jason.slusarchuk@gmail.com>
2017-10-17 15:43:08 -0700

    refactored #generate_areas into multiple methods

--- game.rb
+++ game.rb
@@ -26,6 +26,7 @@ class DND
   def run
     welcome
     generate_areas
+    binding.pry
     create_players
     set_players_in_starting_area_and_location
     set_current_player
@@ -52,34 +53,41 @@ class DND
 
   def generate_areas
     areas_data = YAML.load_file('resources/areas.yml')
-    locations_data = YAML.load_file('resources/locations.yml')
-    event_data = YAML.load_file('resources/events.yml')
 
     areas_data.each do |area_id, desc|
       new_area = Area.new
       new_area.id = area_id
       new_area.description = desc
-
-      locations_data.each do |loc_id, loc_details|
-        if loc_details['area'] == area_id
-          new_loc = Location.new
-          new_loc.id = loc_id
-          new_loc.area_id = area_id
-          new_loc.description = loc_details['description']
-          new_loc.display_name = loc_details['display_name']
-          new_loc.paths = loc_details['paths'].split(' ')
-
-          events = event_data.select do |ev_id, ev_details|
-            ev_details['location'] == loc_id
-          end
-          events.each do |ev_id, ev_details|
-            new_loc.events[ev_id] = ev_details
-          end
-
-          new_area.locations[loc_id] = new_loc
-        end
-      end
+      add_location_data_to(new_area)
       areas[area_id] = new_area
+    end
+  end
+
+  def add_location_data_to(area)
+    locations_data = YAML.load_file('resources/locations.yml')
+
+    locations_data.each do |loc_id, loc_details|
+      if loc_details['area'] == area.id
+        new_loc = Location.new
+        new_loc.id = loc_id
+        new_loc.area_id = area.id
+        new_loc.description = loc_details['description']
+        new_loc.display_name = loc_details['display_name']
+        new_loc.paths = loc_details['paths'].split(' ')
+        add_event_data_to(new_loc)
+        area.locations[loc_id] = new_loc
+      end
+    end
+  end
+
+  def add_event_data_to(location)
+    event_data = YAML.load_file('resources/events.yml')
+
+    events = event_data.select do |ev_id, ev_details|
+      ev_details['location'] == location.id
+    end
+    events.each do |ev_id, ev_details|
+      location.events[ev_id] = ev_details
     end
   end
 
